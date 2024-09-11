5ce5115ba16491a765f673b50bc88785d9fd3c02
Aderlangio123 <faderlangio123@gmail.com>
2024-09-11 15:37:57 -0300

    atividade '13 - numeros negativo postivo ou zero

---
+++ atividade12.py
@@ -0,0 +1,13 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+
+numero = float(input("insira qualquer numero !!  "))
+
+if numero > 0:
+    print(f"{numero} é positivo")
+
+if numero == 0:
+    print(f"{numero} é zero!!")
+
+if numero < 0:
+    print(f"{numero} é negativo!!")
+    
