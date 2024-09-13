747d539b9286658755d2ab6b271f15eb0f52497b
Aderlangio123 <faderlangio123@gmail.com>
2024-09-13 11:38:48 -0300

    atividade 13 - idades e faixa etarias

--- atividade13.py
+++ atividade13.py
@@ -1,13 +1,23 @@
-# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+# Criança (0-12 anos)
+# Adolescente (13-17 anos)
+# Adulto (18-59 anos)
+# Idoso (60 anos ou mais)
 
-numero = float(input("insira qualquer numero !!  "))
 
-if numero > 0:
-    print(f"{numero} é positivo")
+idade = int(input("digite sua idade! :"))
 
-if numero == 0:
-    print(f"{numero} é zero!!")
 
-if numero < 0:
-    print(f"{numero} é negativo!!")
+if idade <= 12:
+    print("voce é uma criança! ")
+
+elif idade >= 13 and idade <= 17 :
+    print("voce é um adolescente!  ")
+
+elif  idade >= 18 and idade <= 59 :
+    print("voce é um adulto!  ")
+
+elif idade >= 60 :
+    print("voce é um idoso!  ")
+    
     
